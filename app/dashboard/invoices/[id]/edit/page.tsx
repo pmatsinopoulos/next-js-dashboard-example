14a3f850cb7591016fa155e4f997812ab6bd5ecf
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2024-04-18 09:05:43 +0300

    Add server side validations

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,42 @@
+import Form from '@/app/ui/invoices/edit-form';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
+const Page = async ({
+  params
+} : {
+  params: {
+    id: string
+  }
+}) => {
+  const id = params.id;
+
+  const [customers, invoice] = await Promise.all([
+    fetchCustomers(),
+    fetchInvoiceById(id),
+  ])
+
+  if (!invoice) {
+    notFound();
+  }
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
+
+export default Page;
---
+++ app/lib/actions.ts
@@ -0,0 +1,133 @@
+'use server';
+
+import Form from '@/app/ui/invoices/create-form';
+import { custom, z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please, select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, { message: 'Please, select an amount greater than 0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please, select an invoice status',
+  }),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+// This is temporary until @types/react-dom is updated
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+}
+
+const createInvoice = async (prevState: State, formData: FormData) => {
+  const validatedFields = CreateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice',
+    }
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  console.log(customerId, amount, status, amountInCents, date);
+
+  try {
+
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+
+  } catch(e) {
+    console.error(e);
+
+    return {
+      message: `Database error when creating invoice: ${e}`
+    }
+  }
+
+  redirect('/dashboard/invoices');
+};
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+const updateInvoice = async (id: string, prevState: State, formData: FormData) => {
+  const validatedFields = UpdateInvoice.safeParse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+
+  } catch(e) {
+    console.error(e);
+    return {
+      message: `Failed to update invoice. Error: ${e}`
+    }
+  }
+
+  redirect('/dashboard/invoices');
+}
+
+const deleteInvoice = async (id: string) => {
+  throw new Error('something went wrong');
+
+  try {
+    await sql`
+      DELETE FROM INVOICES WHERE id = ${id}
+    `
+
+    revalidatePath('/dashboard/invoices');
+  } catch(e) {
+    console.error(e);
+
+    return {
+      message: `Failed to delete invoice. Error: ${e}`
+    }
+  }
+}
+
+export { createInvoice };
+export { deleteInvoice };
+export { updateInvoice };
